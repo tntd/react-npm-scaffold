153af36185bdd8528642232a2e5e4337c19fb1e3
rookie1989 <nichunlong@gmail.com>
2024-09-12 15:57:54 +0800

    new init

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC, ReactNode } from "react";
+import cn from "classnames";
+import "./index.scss";
+
+interface BaseButtonProps {
+    className?: string;
+    size?: "small" | "middle" | "large";
+    disabled?: boolean;
+    children?: ReactNode;
+}
+
+type ButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
+const Index: FC<ButtonProps> = (props) => {
+    const {
+        size = "middle",
+        children = "按钮",
+        className,
+        disabled,
+        ...restProps
+    } = props;
+
+    return (
+        <button
+            className={cn("btn", `btn-${size}`, className, { disabled })}
+            {...restProps}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Index;
--- src/lib/index.js
+++
@@ -1,3 +0,0 @@
-import TextInput from "./components/TextInput";
-
-export { TextInput };
